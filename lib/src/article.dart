d8defd514c2a25b4ca172be40eb2ff064aaeec5f
Ivan Rybalko <ivribalko@gmail.com>
2020-06-21 13:22:15 -0400

    show article

--- lib/src/article.dart
+++ lib/src/article.dart
@@ -1,11 +1,20 @@
+import 'package:html/parser.dart';
+import 'package:http/http.dart';
+
 class ArticleModel {
-  final Future<Article> data = Future.delayed(
-    Duration(seconds: 1),
-  ).then(
-    (_) => Article(),
-  );
+  static const String Root = 'https://evo-lutio.livejournal.com/';
+  static const String Address = '1184227.html';
+  static const String Content = 'div.aentry-post__text.aentry-post__text--view';
+
+  Future<Article> get data => Client()
+      .get(Root + Address)
+      .then((value) => parse(value.body))
+      .then((value) => value.querySelector(Content).text)
+      .then((value) => Article(value));
 }
 
 class Article {
-  String text = 'result';
+  final String text;
+
+  Article(this.text);
 }
