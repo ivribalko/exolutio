52aa54c494d0f165ad86d1997f9581ac5bdd1f07
Ivan Rybalko <ivribalko@gmail.com>
2020-06-21 15:14:56 -0400

    open links

--- lib/ui/article/screen.dart
+++ lib/ui/article/screen.dart
@@ -2,17 +2,21 @@ import 'package:evotexto/src/model.dart';
 import 'package:flutter/material.dart';
 
 class ArticleScreen extends StatelessWidget {
+  ArticleScreen(this.data);
+
+  final Article data;
+
   @override
   Widget build(BuildContext context) {
-    final Article args = ModalRoute.of(context).settings.arguments;
-
-    return SafeArea(
-      child: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Text(args.text),
-            Text(args.comments),
-          ],
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Text(data.text),
+              Text(data.comments),
+            ],
+          ),
         ),
       ),
     );
