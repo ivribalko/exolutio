6dd3e82af13e7f93b0cb0999814d96e571dade93
Ivan Rybalko <ivribalko@gmail.com>
2020-06-23 16:09:26 -0400

    everything

--- lib/ui/common.dart
+++ lib/ui/common.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
+
 const double AppBarHeight = 120;
+
+class SliverProgressIndicator extends StatelessWidget {
+  const SliverProgressIndicator({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: Container(
+        height: 300,
+        child: Center(child: CircularProgressIndicator()),
+      ),
+    );
+  }
+}
