23b20083252599c4c951eca4e721e03bdf9f95f8
Ivan Rybalko <ivribalko@gmail.com>
2020-06-22 22:23:51 -0400

    switch seamlessly

--- lib/ui/home/screen.dart
+++ lib/ui/home/screen.dart
@@ -6,7 +6,7 @@ import '../../main.dart';
 import '../common.dart';
 
 class HomeScreen extends StatelessWidget {
-  final model = locator<Model>();
+  final _model = locator<Model>();
 
   @override
   Widget build(BuildContext context) {
@@ -14,52 +14,72 @@ class HomeScreen extends StatelessWidget {
       body: Selector<Model, bool>(
         selector: (_, Model model) => model.mail,
         builder: (_, bool mail, __) {
-          return _buildCustomScrollView(context);
+          return CustomScrollView(
+            slivers: [
+              _buildSliverAppBar(),
+              StreamBuilder<List<Link>>(
+                stream: _filteredArticles(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return _buildList(context, snapshot.data);
+                  } else {
+                    return _buildLoading();
+                  }
+                },
+              ),
+            ],
+          );
         },
       ),
     );
   }
 
-  CustomScrollView _buildCustomScrollView(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        SliverAppBar(
-          leading: FlatButton.icon(
-            onPressed: () => model.mail = true,
-            icon: Icon(model.mail ? Icons.mail : Icons.mail_outline),
-            label: Container(),
-          ),
-          actions: <Widget>[
-            FlatButton.icon(
-              onPressed: () => model.mail = false,
-              icon: Icon(model.mail ? Icons.info_outline : Icons.info),
-              label: Container(),
-            ),
-          ],
-          expandedHeight: AppBarHeight,
-          flexibleSpace: FlexibleSpaceBar(
-            title: Text(
-              'Эволюция:\n${model.mail ? 'Письма' : 'Прочее'}',
-              textAlign: TextAlign.center,
-            ),
-            centerTitle: true,
-          ),
-          centerTitle: true,
-        ),
-        SliverToBoxAdapter(
-          child: Container(
-            height: 300,
-            child: Center(child: CircularProgressIndicator()),
-          ),
+  SliverAppBar _buildSliverAppBar() {
+    return SliverAppBar(
+      leading: FlatButton.icon(
+        onPressed: () => _model.mail = true,
+        icon: Icon(_model.mail ? Icons.mail : Icons.mail_outline),
+        label: Container(),
+      ),
+      actions: <Widget>[
+        FlatButton.icon(
+          onPressed: () => _model.mail = false,
+          icon: Icon(_model.mail ? Icons.info_outline : Icons.info),
+          label: Container(),
         ),
       ],
+      expandedHeight: AppBarHeight,
+      flexibleSpace: FlexibleSpaceBar(
+        title: Text(
+          'Эволюция:\n${_model.mail ? 'Письма' : 'Прочее'}',
+          textAlign: TextAlign.center,
+        ),
+        centerTitle: true,
+      ),
+      centerTitle: true,
+    );
+  }
+
+  Stream<List<Link>> _filteredArticles() {
+    return _model.mail ? _model.letters.asStream() : _model.others.asStream();
+  }
+
+  Widget _buildLoading() {
+    return SliverToBoxAdapter(
+      child: Container(
+        height: 300,
+        child: Center(child: CircularProgressIndicator()),
+      ),
     );
   }
 
   Widget _buildList(BuildContext context, List<Link> data) {
     return SliverList(
-      delegate: SliverChildListDelegate(
-          data.map((e) => _buildLinkView(context, e)).toList()),
+      delegate: SliverChildListDelegate(data
+          .map(
+            (e) => _buildLinkView(context, e),
+          )
+          .toList()),
     );
   }
 
