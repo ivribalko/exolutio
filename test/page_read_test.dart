7e21de8f62142247526397236bcdb76fd778d9b7
Ivan Rybalko <ivribalko@gmail.com>
2020-06-27 19:50:01 -0400

    read test html files

--- test/page_read_test.dart
+++ test/page_read_test.dart
@@ -1,21 +1,46 @@
+import 'dart:async';
 import 'dart:io';
 
+import 'package:exolutio/src/loader.dart';
 import 'package:exolutio/src/model.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+class MockLoader extends Mock implements Loader {}
+
 class MockPreferences extends Mock implements SharedPreferences {}
 
 void main() {
-  test('comments count is 42', () {
-    final html = File.fromUri(
-      Uri.file('evo-lutio.livejournal.com_1185261.html'),
-    );
-    final model = Model(MockPreferences());
-    final link = model[Tag.letters].first;
-    final article = model.article(link) as Article;
+  Model model;
+  var updated;
 
-    expect(42, article.comments.length);
+  setUp(() {
+    final loader = MockLoader();
+
+    when(loader.page(any)).thenAnswer((_) => File(
+          'test/evo-lutio.livejournal.com.html',
+        ).readAsString());
+
+    when(loader.body(any)).thenAnswer((_) => File(
+          'test/evo-lutio.livejournal.com_1185261.html',
+        ).readAsString());
+
+    updated = StreamController();
+    model = Model(loader, MockPreferences());
+    model.addListener(() => updated.add(null));
+  });
+  tearDown(() {
+    updated.close();
+    model.dispose();
+  });
+
+  test('comments count is 42', () async {
+    model.loadMore();
+    await updated.stream.first;
+    final link = model[Tag.letters].first;
+    final article = await model.article(link);
+
+    expect(article.comments.length, equals(42));
   });
 }
